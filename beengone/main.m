196fad13d649b95d0daa406909a120665625ed14
Brett Terpstra <brett.terpstra@oracle.com>
2024-12-22 06:39:49 -0600

    Initial Commit

---
+++ beengone/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  beengone
+//
+//  Created by Brett Terpstra on 12/22/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
