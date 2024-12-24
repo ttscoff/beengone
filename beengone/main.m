a3241627553182b9e1242f2b7e7e9c9eeb79f762
Brett Terpstra <brett.terpstra@oracle.com>
2024-12-24 06:01:26 -0600

    formatting

--- beengone/main.m
+++ beengone/main.m
@@ -34,10 +34,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include <Cocoa/Cocoa.h>
 #include <CoreFoundation/CoreFoundation.h>
 #include <CoreServices/CoreServices.h>
 #include <IOKit/IOKitLib.h>
-#include <Cocoa/Cocoa.h>
 
 #include "argparse.h"
 
@@ -45,22 +45,21 @@
 
 /******************************************************************************/
 
-@interface IdleTime: NSObject
-{
+@interface IdleTime : NSObject {
 @protected
 
-    mach_port_t   _ioPort;
-    io_iterator_t _ioIterator;
-    io_object_t   _ioObject;
+  mach_port_t _ioPort;
+  io_iterator_t _ioIterator;
+  io_object_t _ioObject;
 
 @private
 
-    id r1;
-    id r2;
+  id r1;
+  id r2;
 }
 
-@property( readonly ) uint64_t   timeIdle;
-@property( readonly ) NSUInteger secondsIdle;
+@property(readonly) uint64_t timeIdle;
+@property(readonly) NSUInteger secondsIdle;
 
 @end
 
@@ -68,148 +67,108 @@
 
 @implementation IdleTime
 
-- ( id )init
-{
-    kern_return_t status = 0;
+- (id)init {
+  kern_return_t status = 0;
 
-    if( ( self = [ super init ] ) )
-    {
-        if (@available(macOS 12.0, *)) {
-            _ioPort = kIOMainPortDefault;
-        } else {
-            printf('%s', "macOS 12.0 or higher required.");
-            exit(1);
-        }
-
-        status = IOServiceGetMatchingServices
-        (
-            _ioPort,
-            IOServiceMatching( "IOHIDSystem" ),
-            &_ioIterator
-        );
-
-        if( status != KERN_SUCCESS )
-        {
-            @throw [ NSException
-                        exceptionWithName:  @"IdleTimeIOHIDError"
-                        reason:             @"Error accessing IOHIDSystem"
-                        userInfo:           nil
-                   ];
-        }
-
-        _ioObject = IOIteratorNext( _ioIterator );
-
-        if( _ioObject == 0 )
-        {
-            IOObjectRelease( _ioIterator );
-
-            @throw [ NSException
-                        exceptionWithName:  @"IdleTimeIteratorError"
-                        reason:             @"Invalid iterator"
-                        userInfo:           nil
-                   ];
-        }
-
-        IOObjectRetain( _ioObject );
-        IOObjectRetain( _ioIterator );
+  if ((self = [super init])) {
+    if (@available(macOS 12.0, *)) {
+      _ioPort = kIOMainPortDefault;
+    } else {
+      printf('%s', "macOS 12.0 or higher required.");
+      exit(1);
     }
 
-    return self;
+    status = IOServiceGetMatchingServices(
+        _ioPort, IOServiceMatching("IOHIDSystem"), &_ioIterator);
+
+    if (status != KERN_SUCCESS) {
+      @throw [NSException exceptionWithName:@"IdleTimeIOHIDError"
+                                     reason:@"Error accessing IOHIDSystem"
+                                   userInfo:nil];
+    }
+
+    _ioObject = IOIteratorNext(_ioIterator);
+
+    if (_ioObject == 0) {
+      IOObjectRelease(_ioIterator);
+
+      @throw [NSException exceptionWithName:@"IdleTimeIteratorError"
+                                     reason:@"Invalid iterator"
+                                   userInfo:nil];
+    }
+
+    IOObjectRetain(_ioObject);
+    IOObjectRetain(_ioIterator);
+  }
+
+  return self;
 }
 
-- ( void )dealloc
-{
-    IOObjectRelease( _ioObject );
-    IOObjectRelease( _ioIterator );
+- (void)dealloc {
+  IOObjectRelease(_ioObject);
+  IOObjectRelease(_ioIterator);
 }
 
-- ( uint64_t )timeIdle
-{
-    kern_return_t          status;
-    CFTypeRef              idle;
-    CFTypeID               type;
-    uint64_t               time;
-    CFMutableDictionaryRef properties;
+- (uint64_t)timeIdle {
+  kern_return_t status;
+  CFTypeRef idle;
+  CFTypeID type;
+  uint64_t time;
+  CFMutableDictionaryRef properties;
 
-    properties = NULL;
-    status     = IORegistryEntryCreateCFProperties
-    (
-        _ioObject,
-        &properties,
-        kCFAllocatorDefault,
-        0
-    );
+  properties = NULL;
+  status = IORegistryEntryCreateCFProperties(_ioObject, &properties,
+                                             kCFAllocatorDefault, 0);
 
-    if( status != KERN_SUCCESS || properties == NULL )
-    {
-        @throw [ NSException
-                    exceptionWithName:  @"IdleTimeSystemPropError"
-                    reason:             @"Cannot get system properties"
-                    userInfo:           nil
-               ];
-    }
+  if (status != KERN_SUCCESS || properties == NULL) {
+    @throw [NSException exceptionWithName:@"IdleTimeSystemPropError"
+                                   reason:@"Cannot get system properties"
+                                 userInfo:nil];
+  }
 
-    idle = CFDictionaryGetValue( properties, CFSTR( "HIDIdleTime" ) );
+  idle = CFDictionaryGetValue(properties, CFSTR("HIDIdleTime"));
 
-    if( !idle )
-    {
-        CFRelease( ( CFTypeRef )properties );
+  if (!idle) {
+    CFRelease((CFTypeRef)properties);
 
-        @throw [ NSException
-                    exceptionWithName:  @"IdleTimeSystemTimeError"
-                    reason:             @"Cannot get system idle time"
-                    userInfo:           nil
-               ];
-    }
+    @throw [NSException exceptionWithName:@"IdleTimeSystemTimeError"
+                                   reason:@"Cannot get system idle time"
+                                 userInfo:nil];
+  }
 
-    CFRetain( idle );
+  CFRetain(idle);
 
-    type = CFGetTypeID( idle );
+  type = CFGetTypeID(idle);
 
-    if( type == CFDataGetTypeID() )
-    {
-        CFDataGetBytes
-        (
-            ( CFDataRef )idle,
-            CFRangeMake( 0, sizeof( time ) ),
-            ( UInt8 * )&time
-        );
+  if (type == CFDataGetTypeID()) {
+    CFDataGetBytes((CFDataRef)idle, CFRangeMake(0, sizeof(time)),
+                   (UInt8 *)&time);
 
-    }
-    else if( type == CFNumberGetTypeID() )
-    {
-        CFNumberGetValue
-        (
-            ( CFNumberRef )idle,
-            kCFNumberSInt64Type,
-            &time
-        );
-    }
-    else
-    {
-        CFRelease( idle );
-        CFRelease( ( CFTypeRef )properties );
+  } else if (type == CFNumberGetTypeID()) {
+    CFNumberGetValue((CFNumberRef)idle, kCFNumberSInt64Type, &time);
+  } else {
+    CFRelease(idle);
+    CFRelease((CFTypeRef)properties);
 
-        @throw [ NSException
-                    exceptionWithName:  @"IdleTimeTypeError"
-                    reason:             [ NSString stringWithFormat: @"Unsupported type: %d\n", ( int )type ]
-                    userInfo:           nil
-               ];
-    }
+    @throw [NSException
+        exceptionWithName:@"IdleTimeTypeError"
+                   reason:[NSString stringWithFormat:@"Unsupported type: %d\n",
+                                                     (int)type]
+                 userInfo:nil];
+  }
 
-    CFRelease( idle );
-    CFRelease( ( CFTypeRef )properties );
+  CFRelease(idle);
+  CFRelease((CFTypeRef)properties);
 
-    return time;
+  return time;
 }
 
-- ( NSUInteger )secondsIdle
-{
-    uint64_t time;
+- (NSUInteger)secondsIdle {
+  uint64_t time;
 
-    time = self.timeIdle;
+  time = self.timeIdle;
 
-    return ( NSUInteger )( time >> 30 );
+  return (NSUInteger)(time >> 30);
 }
 
 @end
@@ -221,100 +180,102 @@ static const char *const usages[] = {
 };
 
 unsigned long parse_time_string(const char *time_str) {
-    unsigned long total_seconds = 0;
-    unsigned long value = 0;
-    char unit = '\0';
+  unsigned long total_seconds = 0;
+  unsigned long value = 0;
+  char unit = '\0';
 
-    while (*time_str) {
-        if (sscanf(time_str, "%lu%c", &value, &unit) == 2) {
-            switch (unit) {
-                case 'd':
-                    total_seconds += value * 86400; // 1 day = 86400 seconds
-                    break;
-                case 'h':
-                    total_seconds += value * 3600; // 1 hour = 3600 seconds
-                    break;
-                case 'm':
-                    total_seconds += value * 60; // 1 minute = 60 seconds
-                    break;
-                case 's':
-                    total_seconds += value;
-                    break;
-                default:
-                    break;
-            }
+  while (*time_str) {
+    if (sscanf(time_str, "%lu%c", &value, &unit) == 2) {
+      switch (unit) {
+      case 'd':
+        total_seconds += value * 86400; // 1 day = 86400 seconds
+        break;
+      case 'h':
+        total_seconds += value * 3600; // 1 hour = 3600 seconds
+        break;
+      case 'm':
+        total_seconds += value * 60; // 1 minute = 60 seconds
+        break;
+      case 's':
+        total_seconds += value;
+        break;
+      default:
+        break;
+      }
 
-            // Advance the pointer past the current value and unit
-            while (*time_str && *time_str != ' ' && *time_str != '\t') {
-                time_str++;
-            }
-        } else if (sscanf(time_str, "%lu", &value) == 1) {
-            total_seconds += value;
-            break;
-        } else {
-            break;
-        }
-
-        // Skip any spaces or tabs
-        while (*time_str && (*time_str == ' ' || *time_str == '\t')) {
-            time_str++;
-        }
+      // Advance the pointer past the current value and unit
+      while (*time_str && *time_str != ' ' && *time_str != '\t') {
+        time_str++;
+      }
+    } else if (sscanf(time_str, "%lu", &value) == 1) {
+      total_seconds += value;
+      break;
+    } else {
+      break;
     }
 
-    return total_seconds;
+    // Skip any spaces or tabs
+    while (*time_str && (*time_str == ' ' || *time_str == '\t')) {
+      time_str++;
+    }
+  }
+
+  return total_seconds;
 }
 
-
-int beengone_version_cb(struct argparse *self, const struct argparse_option *option) {
-    printf("beengone version %s\n", BEENGONE_VERSION);
-    exit(0);
+int beengone_version_cb(struct argparse *self,
+                        const struct argparse_option *option) {
+  printf("beengone version %s\n", BEENGONE_VERSION);
+  exit(0);
 }
 
-int main( int argc, char * argv[] )
-{
-    @autoreleasepool {
-        int newline = 0;
-        unsigned long limit = -1;
-        const char *minimum = NULL;
+int main(int argc, char *argv[]) {
+  @autoreleasepool {
+    int newline = 0;
+    unsigned long limit = 0;
+    const char *minimum = NULL;
 
-        struct argparse_option options[] = {
-            OPT_HELP(),
-            OPT_GROUP("Basic options"),
-            OPT_BOOLEAN('n', "no-newline", &newline, "print without newline", NULL, 0, OPT_NONEG),
-            OPT_STRING('m', "minimum", &minimum, "test for minimum idle time in seconds, exit 0 or 1 based on condition. Accepts strings like 5h 30m or 1d12h", NULL, 0, OPT_NONEG),
-            OPT_BOOLEAN('v', "version", NULL, "show version and exit", beengone_version_cb, 0, OPT_NONEG),
-            OPT_END(),
-        };
+    struct argparse_option options[] = {
+        OPT_HELP(),
+        OPT_GROUP("Basic options"),
+        OPT_BOOLEAN('n', "no-newline", &newline, "print without newline", NULL,
+                    0, OPT_NONEG),
+        OPT_STRING('m', "minimum", &minimum,
+                   "test for minimum idle time in seconds, exit 0 or 1 based "
+                   "on condition. Accepts strings like 5h 30m or 1d12h",
+                   NULL, 0, OPT_NONEG),
+        OPT_BOOLEAN('v', "version", NULL, "show version and exit",
+                    beengone_version_cb, 0, OPT_NONEG),
+        OPT_END(),
+    };
 
-        struct argparse argparse;
-        argparse_init(&argparse, options, usages, 0);
-        argparse_describe(&argparse, "\nPrint the system idle time in seconds", "");
-        argc = argparse_parse(&argparse, argc, argv);
+    struct argparse argparse;
+    argparse_init(&argparse, options, usages, 0);
+    argparse_describe(&argparse, "\nPrint the system idle time in seconds", "");
+    argc = argparse_parse(&argparse, argc, argv);
 
-        if (minimum != NULL) {
-            limit = parse_time_string(minimum);
-        }
-        
-        unsigned long idle = (unsigned long)[[[IdleTime alloc] init] secondsIdle];
-        
-        if (limit >= 0) {
-            fprintf(stderr, "Limit: %lu seconds, idle time %lu: ", limit, idle);
-            if (idle >= limit) {
-                fprintf(stderr, "%s", "true\n");
-                return EXIT_SUCCESS;
-            } else {
-                fprintf(stderr, "%s", "false\n");
-                return EXIT_FAILURE;
-            }
-        }
-
-        if (newline != 0)
-            printf("%lu", idle);
-        else
-            printf("%lu\n", idle);
-
+    if (minimum != NULL) {
+      limit = parse_time_string(minimum);
     }
 
-    return EXIT_SUCCESS;
-}
+    unsigned long idle = (unsigned long)[[[IdleTime alloc] init] secondsIdle];
 
+    if (limit > 0) {
+      fprintf(stderr, "Limit: %lu seconds, idle time %lu: ", limit, idle);
+      if (idle >= limit) {
+        fprintf(stderr, "%s", "true\n");
+        return EXIT_SUCCESS;
+      } else {
+        fprintf(stderr, "%s", "false\n");
+        return EXIT_FAILURE;
+      }
+    }
+
+    if (newline != 0)
+      printf("%lu", idle);
+    else
+      printf("%lu\n", idle);
+  }
+
+  return EXIT_SUCCESS;
+}
